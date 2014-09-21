1a7139ce7a038d72b89f59fae4451e211eed9498
Evaldo Junior <evaldoapjunior@gmail.com>
2014-09-20 21:06:57 -0300

    Alteração da página de Programação
    Alteração da página de Programação

--- programa.php
+++ programa.php
@@ -1,1 +1,23 @@
-teste
+<?php include  "inc_header.php"; ?>
+
+
+			<!-- Inner Page Content Start -->
+			
+			<div class="inner-page">
+			
+				<!-- INNER PAGE BANNER START -->
+				
+				<div class="inner-page-banner padd parallax-banner">
+					<div class="container">
+						<!-- Heading -->
+						<h3>Patrocínio</h3>
+						<hr />
+						<!-- Paragraph -->
+						<p>Programação a Ser Definida.</p>
+					</div>
+				</div>
+
+			</div>
+			
+
+<?php include  "inc_footer.php"; ?>

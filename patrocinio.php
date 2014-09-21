e99c8800346800bb88c9891ea771576859ec34e8
Evaldo Junior <evaldoapjunior@gmail.com>
2014-09-20 21:07:58 -0300

    Alteração da página de Patrocícinio
    Alteração da página de Patrocícinio

--- patrocinio.php
+++ patrocinio.php
@@ -13,23 +13,11 @@
 						<h3>Patrocínio</h3>
 						<hr />
 						<!-- Paragraph -->
-						<p>O Café Brasil 2014 espera receber mais de 800 pessoas de todo o Brasil. Se você tem interesse em divulgar a sua empresa, produtos e/ou serviços às pessoas mais envolvidas com desenvolvimento da plataforma Java, venha patrocinar o evento. Oferecemos diversos níveis de patrocínio com excelentes benefícios. Obtenha mais informações através do email naoexiste@soujava.org.br.</p>
+						<p>O Café Brasil 2014 espera receber mais de 800 pessoas de todo o Brasil. Se você tem interesse em divulgar a sua empresa, produtos e/ou serviços às pessoas mais envolvidas com desenvolvimento da plataforma Java, venha patrocinar o evento. Oferecemos diversos níveis de patrocínio com excelentes benefícios. Obtenha mais informações através do email eventos@soujava.org.br.</p>
 					</div>
 				</div>
 				
-				<!-- INNER PAGE BANNER END -->
-				
-				<div class="aboutus">		
-					<!-- Inner Page Experience -->
-					<div class="inner-experience padd">
-						<div class="container">
-							<!-- Heading -->
-							<h2>Para maiores informacoes sobre patrocinio, entre em contato com eventos@soujava.org.br</h2>
-						</div>
-						<!-- Inner Page Experience End -->
-					</div>
-				</div>
-				
+
 			</div>
 			
 

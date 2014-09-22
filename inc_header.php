9400af50cf1b0798986fa63bb3ca0e9b7ba85dc6
Evaldo Junior <evaldoapjunior@gmail.com>
2014-09-22 17:14:06 -0300

    Alteração menu
    Alteração menu

--- inc_header.php
+++ inc_header.php
@@ -51,45 +51,50 @@
                 <nav class="navbar navbar-default" role="navigation">
                     <div class="container">
                         <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="#"><span class="logo"><img src="img/logo-cafebrasil.png" /></span></a>
-                        </div>
-
+                        <table border ="1">
+                        <tr>
+                        <td><div class="navbar-header">
+                            	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	                                <span class="sr-only">Toggle navigation</span>
+	                                <span class="icon-bar"></span>
+	                                <span class="icon-bar"></span>
+	                                <span class="icon-bar"></span>
+	                        </button>
+                            	<a class="navbar-brand" href="#"><span class="logo"><img src="img/logo-cafebrasil.png" /></span></a>
+                            </div>
+                        </td>    
+			<td>
                         <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse adjust_header" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right adjust_header">
-                                <li><a href="index">Início</a></li>
-                                
-                                <li class="dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Informações<span class="fa fa-angle-down"></span></a>
-					<ul class="dropdown-menu" role="menu">
-					<li><a href="sobre">Sobre o evento</a></li>
-					<li><a href="soujava">Sobre o SouJava</a></li>
-					<li><a href="programa">Programa</a></li>
-					<li><a href="local">Data e Local</a></li>
-                                	<li><a href="http://eventos.soujava.org.br/evt/cafebrasil/cb2014/schedConf/cfp">CFP</a></li>
-				</ul>
-				</li>
-				
-				<li class="dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Patrocínio <span class="fa fa-angle-down"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="patrocinio">Patrocinadores</a></li>
-						<li><a href="patrocinio">Seja um Patrocinador</a></li>
+	                        <div class="collapse navbar-collapse adjust_header" id="bs-example-navbar-collapse-1">
+	                            <ul class="nav navbar-nav navbar-right adjust_header">
+	                                <li><a href="index">Início</a></li>
+	                                
+	                                <li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">Informações<span class="fa fa-angle-down"></span></a>
+						<ul class="dropdown-menu" role="menu">
+						<li><a href="sobre">Sobre o evento</a></li>
+						<li><a href="soujava">Sobre o SouJava</a></li>
+						<li><a href="programa">Programa</a></li>
+						<li><a href="local">Data e Local</a></li>
+	                                	<li><a href="http://eventos.soujava.org.br/evt/cafebrasil/cb2014/schedConf/cfp">CFP</a></li>
 					</ul>
-				</li>
-				<li><a href="http://eventos.soujava.org.br/evt/cafebrasil/cb2014/schedConf/registration">Inscreva-se</a></li>
-                                <!-- <li><a href="faq">FAQ</a></li> -->
-                                <li><a href="contato">Contato</a></li>
-                                <li><a href="http://www.soujava.org.br" target="_blank"><img src="img/menu_soujava.png"/></a></li>
-                            </ul>
-                        </div><!-- /.navbar-collapse -->
+					</li>
+					
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">Patrocínio <span class="fa fa-angle-down"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="patrocinio">Patrocinadores</a></li>
+							<li><a href="patrocinio">Seja um Patrocinador</a></li>
+						</ul>
+					</li>
+					<li><a href="http://eventos.soujava.org.br/evt/cafebrasil/cb2014/schedConf/registration">Inscreva-se</a></li>
+	                                <!-- <li><a href="faq">FAQ</a></li> -->
+	                                <li><a href="contato">Contato</a></li>
+	                                <li><a href="http://www.soujava.org.br" target="_blank"><img src="img/menu_soujava.png"/></a></li>
+	                            </ul>
+	                        </div><!-- /.navbar-collapse -->
+	                </td>        
+	                </tr>        
                     </div>
                 </nav>
             </div>
